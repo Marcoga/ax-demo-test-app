2a416f861ea2b81b084ae5213be718f467de689b
Marco Garcia <marco.gar7@gmail.com>
2018-10-23 20:17:22 +0200

    Reverted fixed height of table

--- src/Components/Watchlist.js
+++ src/Components/Watchlist.js
@@ -66,9 +66,6 @@ export default class Watchlist extends React.Component {
         </div>
         <Divider />
         <Table
-          scroll={{
-            y: window.innerHeight - 275
-          }}
           onChange={this.handleChange}
           columns={[
             {
