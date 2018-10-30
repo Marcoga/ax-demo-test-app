573f80d8823693d4c9cf0b5c1a769efaead3a2cd
Marco Garcia <marco.gar7@gmail.com>
2018-10-30 22:55:59 +0100

    Add test script

---
+++ src/runTests.js
@@ -0,0 +1,16 @@
+const url = `${process.env.AXCEPT_SERVICE_URL}/test-mgmt/test-run/${
+  process.env.AXCEPT_ENV_ID
+}`;
+fetch(url, {
+  method: "POST", // *GET, POST, PUT, DELETE, etc.
+  mode: "cors", // no-cors, cors, *same-origin
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+    "a-token": process.env.AX_CI_AUTH_TOKEN
+    // "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: JSON.stringify({
+    triggeredBy: process.env.TRIGGERED_BY,
+    clientVersion: process.env.CLIENT_VERSION
+  }) // body data type must match "Content-Type" header
+}).then(response => response.json());
